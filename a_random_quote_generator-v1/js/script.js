6b181779e4cc3cd8ff6c299b014fe0ba933a68eb
msturn2 <82980444+msturn2@users.noreply.github.com>
2021-05-17 03:04:35 -0700

    Super close, not sure where the problem lies.
    I'm not getting error messages in the Console anymore (thank God)....I'm a little lost on how to button this up and get the web page to work.  I will post on Slack in the morning.

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -10,20 +10,62 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+const quotes = [
+  {
+    quote: "You can observe a lot just by watching.",
+    source: "Yogi Berra",
+    citation: "Berra, Yogi (1998). The Yogi Book. New York: Workman Publishing. p. 9.",
+    year: "1998"
+  },
+  {
+    quote: "If you're going through hell, keep going.",
+    source: "Winston Spencer Churchill",
+    citation: "Robert M. Langworth. Finest Hour, The Journal of Winston Churchill",
+    year: "1940"
+  },
+  {
+    quote: "Cry 'Havoc!', and let slip the dogs of war.",
+    source: "William Shakespeare",
+    citation: "phrase spoken by Mark Atony in Act 3, Scene 1, line 273 of English playwright William Shakespeare's Julius Caesar",
+    year: "1599 - 1600"
+  },
+  {
+    quote: "VENI VINI VICI",
+    source: "Julius Caesar",
+    citation: "According to Suetonius, Caesar displayed the words veni vidi vici in his triumph held over Pontus.",
+    year: "46 B.C." 
+  },
+  {
+    quote: "Now, I am become Death, the destroyer of worlds.",
+    source: "Bhagavad Gita",
+    citation: "https://en.wikipedia.org/wiki/J._Robert_Oppenheimer",
+    year: "unknown"
+  }
+];
 
 
 /***
  * `getRandomQuote` function
 ***/
-
-
-
+function getRandomQuote() {
+  let randomNumber = Math.floor(Math.random() * quotes.length);
+  for (let i = 0; i < quotes.length; i++) {
+    return randomQuote = quotes[randomNumber];
+  }
+}
+const result = getRandomQuote(quotes);
 /***
  * `printQuote` function
 ***/
-
-
+function printQuote() {
+  let html = "";
+  html = "<p class='quote'>" + result.quote + "</p>";
+  html = "<p class='source'>" + result.source;
+  html = "<spanp class='citation'>" + result.citation + "</span>";
+  html = "<span class='year'>" + result.year + "</span>";
+  html = "</p>";
+}
+printQuote(result);
 
 /***
  * click event listener for the print quote button
